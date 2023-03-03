83e95350e4e96f690b5c08363d299360d2fb7846
Douglas <douglastsp@gmail.com>
2023-03-02 23:27:13 -0300

    login and register view have been created

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -10,7 +10,16 @@ defineProps({
 <template>
     <button
         :type="type"
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        class="
+            bg-blue-500
+            hover:bg-blue-600
+            text-white
+            font-bold
+            text-[23px]
+            p-2.5
+            m-5
+            rounded-lg
+        "
     >
         <slot />
     </button>
